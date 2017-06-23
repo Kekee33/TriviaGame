0d73623df0416182815546c097ca5248c58cd950
keisha <keisha@sample.com>
2017-06-23 11:02:14 -0400

    updated html and js functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 var number = 30
 
 function run() {
-	var interval = setInterval(decrease, 1000);
+	window.timer = setInterval(decrease, 1000);
 }
 
 function decrease(){
@@ -11,43 +11,51 @@ function decrease(){
 
 	if (number === 0) {
 	    alert("Time is Up!");
+	    clearInterval(window.timer);
 	}
 }
 run();
 
-
-
-function checkAnsw(){
-	$("#button").on("click", checkAnsw);
-	console.log("yay!");
-	}
-	var question1 = document.quiz.question1.value;
-    //var question2 = document.quiz.question2.value;
-	// var question3 = document.quiz.question3.value;
-	// var question4 = document.quiz.question4.value;
-	// var question5 = document.quiz.question5.value;
+function checkAnswers(){
 	var correct = 0;
 
- 	if (question1 === "Uma Thurman") {
- 		console.log("Good Job!");
-// }};
-// 	if (question2 === "Bruce Lee") {
-// 			//correct++
-// 	//}
-// 	if (question3 === "O-Ren Ishii") {
-// 			correct++
-// 	}
-// 	if (question4 === "Hatori Hanzo") {
-// 			correct++
-// 	}
-// 	if (question5 === "Samuel L. Jackson") {
-// 			correct++
-// 	}
+	var correct_answers = {
+		question1: "Uma Thurman",
+		question2: "Bruce Lee",
+		question3: "O-Ren Ishii",
+		question4: "Hattori Hanzo", 
+		question5: "Samuel L. Jackson",
 
-// $("#numCorrect").html("Good job " + correct + " correct");
+	};
 
- checkAnsw()
+$('input').each(function() {
+	var question = $(this).attr('name');
+
+	if ( $(this).prop('checked') ){
+		var user_answer = $(this).val();
+
+		if (correct_answers[question] == user_answer)
+
+			correct++;
+	}	
+});
+
+$("#numCorrect").text("Good job " + correct + " correct");
+
+// $(window).scrollTop(0);
+
+function init(){
+	$("#button").on("click", checkAnswers);
+}
+
+init();	
+}
+
+
+
+
+	
  //$("#button").on("click")(checkAnsw);
-// alert("you won!");
 
- }
+
+// }
